ba76a7c88410aad8019a51077b7d1c2225153824
Dionei Bianchati <dion.biancha@gmail.com>
2021-01-16 19:44:48 -0300

    Route ajust

--- src/Routes.js
+++ src/Routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
@@ -7,7 +7,10 @@ import NotFound from './pages/NotFound';
 export default () => {
   return (
     <Switch>
-      <Route exact path="/personal-website/">
+      <Route exact path="/personal-website">
+        <Redirect to="/"/>
+      </Route>
+      <Route exact path="/">
         <Home />
       </Route>
       <Route>
